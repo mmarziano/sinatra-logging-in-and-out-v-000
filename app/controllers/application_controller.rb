6986ca2dec0a7d31ed22ae29d9d48809a37ad20c
Monica Marziano <mmarziano1@gmail.com>
2019-05-15 20:51:05 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,7 @@ class ApplicationController < Sinatra::Base
   post '/login' do
     
     @user = User.find_by('username' => params[:username])
-    session[:user_id] == @user.id
+    session[:user_id] = @user.id
     binding.pry
     redirect to '/account'
 
