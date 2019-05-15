5ca5b7ccd0c1eb14bc303c25eb043444920aa6eb
Monica Marziano <mmarziano1@gmail.com>
2019-05-15 20:47:37 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,11 +11,18 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
+    
+    @user = User.find_by('username' => params[:username])
+    session[:user_id] == @user.id
+    redirect to '/account'
 
   end
 
   get '/account' do
-
+    binding.pry
+    if Helpers.is_logged_in?
+      erb :account
+    end
   end
 
   get '/logout' do
--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,14 @@
-class Helpers
+class Helpers 
+  
+  def self.current_user(session)
+    if @user.id == session[:user_id]
+      return true 
+    end 
+  end 
+  
+  def self.is_logged_in?(session)
+    if @user.id == session[:user_id]
+      return true 
+    end 
+  end 
 end
